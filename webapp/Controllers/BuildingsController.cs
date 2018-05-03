d0e4b13b7f9d2fce96ecb738482161b2e20799ff
Raul Velcherean <raul.velc@gmail.com>
2018-05-03 14:09:59 +0300

    Papadii

---
+++ webapp/Controllers/BuildingsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using webapp.Models;
+
+namespace webapp.Controllers
+{
+    public class BuildingsController : Controller
+    {
+        private EMA db = new EMA();
+
+        // GET: Buildings
+        public ActionResult Index()
+        {
+            var buildings = db.Buildings.Include(b => b.City);
+            return View(buildings.ToList());
+        }
+
+        // GET: Buildings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Building building = db.Buildings.Find(id);
+            if (building == null)
+            {
+                return HttpNotFound();
+            }
+            return View(building);
+        }
+
+        // GET: Buildings/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_city = new SelectList(db.Cities, "id_city", "name");
+            return View();
+        }
+
+        // POST: Buildings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_building,id_city,name,address")] Building building)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Buildings.Add(building);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_city = new SelectList(db.Cities, "id_city", "name", building.id_city);
+            return View(building);
+        }
+
+        // GET: Buildings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Building building = db.Buildings.Find(id);
+            if (building == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_city = new SelectList(db.Cities, "id_city", "name", building.id_city);
+            return View(building);
+        }
+
+        // POST: Buildings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_building,id_city,name,address")] Building building)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(building).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_city = new SelectList(db.Cities, "id_city", "name", building.id_city);
+            return View(building);
+        }
+
+        // GET: Buildings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Building building = db.Buildings.Find(id);
+            if (building == null)
+            {
+                return HttpNotFound();
+            }
+            return View(building);
+        }
+
+        // POST: Buildings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Building building = db.Buildings.Find(id);
+            db.Buildings.Remove(building);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- webapp/Controllers/EventsController.cs
+++ webapp/Controllers/EventsController.cs
@@ -1,44 +1,142 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using webapp.Models;
-
 
 namespace webapp.Controllers
 {
     public class EventsController : Controller
     {
+        private EMA db = new EMA();
+
         // GET: Events
         public ActionResult Index()
         {
+            var events = db.Events;
+            //var events = db.Events.Include(@ => @.Artist).Include(@ => @.Building).Include(@ => @.Event_Type);
+            return View(events.ToList());
+           // return View();
+        }
+
+        // GET: Events/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@event);
+        }
+
+        // GET: Events/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_artist = new SelectList(db.Artists, "id_artist", "name");
+            ViewBag.id_building = new SelectList(db.Buildings, "id_building", "name");
+            ViewBag.id_etype = new SelectList(db.Event_Type, "id_etype", "name");
             return View();
         }
 
-        // /events/addevent
-        public ActionResult AddEvent()
+        // POST: Events/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_event,id_building,name,date,description,id_artist,id_etype")] Event @event)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                db.Events.Add(@event);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_artist = new SelectList(db.Artists, "id_artist", "name", @event.id_artist);
+            ViewBag.id_building = new SelectList(db.Buildings, "id_building", "name", @event.id_building);
+            ViewBag.id_etype = new SelectList(db.Event_Type, "id_etype", "name", @event.id_etype);
+            return View(@event);
         }
 
-        // /events/delevent
-        public ActionResult DelEvent()
+        // GET: Events/Edit/5
+        public ActionResult Edit(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_artist = new SelectList(db.Artists, "id_artist", "name", @event.id_artist);
+            ViewBag.id_building = new SelectList(db.Buildings, "id_building", "name", @event.id_building);
+            ViewBag.id_etype = new SelectList(db.Event_Type, "id_etype", "name", @event.id_etype);
+            return View(@event);
         }
 
-        // /events/editevent/index
-        public ActionResult EditEvent()
+        // POST: Events/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_event,id_building,name,date,description,id_artist,id_etype")] Event @event)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                db.Entry(@event).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_artist = new SelectList(db.Artists, "id_artist", "name", @event.id_artist);
+            ViewBag.id_building = new SelectList(db.Buildings, "id_building", "name", @event.id_building);
+            ViewBag.id_etype = new SelectList(db.Event_Type, "id_etype", "name", @event.id_etype);
+            return View(@event);
         }
 
-        // /events/viewall
-
-        public ActionResult ViewEvents()
+        // GET: Events/Delete/5
+        public ActionResult Delete(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@event);
+        }
+
+        // POST: Events/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Event @event = db.Events.Find(id);
+            db.Events.Remove(@event);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
