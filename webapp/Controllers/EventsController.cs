79882e0bb5bd0b5cd1706d2d4338c184e76f1bec
Alexandra Mustea <ale_alexandra96@hotmail.com>
2018-05-17 22:35:54 +0300

    //am facut search-ul

--- webapp/Controllers/EventsController.cs
+++ webapp/Controllers/EventsController.cs
@@ -136,5 +136,89 @@ namespace webapp.Controllers
             }
             base.Dispose(disposing);
         }
+        public ActionResult SearchByArtist(String id)
+        {
+            var events = db.Events;
+            var artisti = db.Artists;
+            List<Event> Users = new List<Event>();
+            List<Artist> artisti1 = new List<Artist>();
+            foreach (Artist a in artisti)
+            {
+                if (a.name == id)
+                {
+                    artisti1.Add(a);
+                }
+            }
+
+            foreach (Event e in events)
+            {
+                foreach (Artist a in artisti1)
+                {
+                    if (e.id_artist == a.id_artist)
+                    {
+                        Users.Add(e);
+                    }
+                }
+            }
+            return View(Users.ToList());
+
+        }
+        public ActionResult SearchByCity(String id)
+        {
+            var events = db.Events;
+            var buildings = db.Buildings;
+            var citys = db.Cities;
+            List<Event> Users = new List<Event>();
+            List<Building> bili = new List<Building>();
+            List<City> cili = new List<City>();
+            foreach (City c in citys)
+            {
+                if (c.name == id)
+                {
+                    cili.Add(c);
+                }
+
+            }
+
+            foreach (Building b in buildings)
+            {
+                foreach (City c in cili)
+                {
+                    if (b.id_city == c.id_city)
+                    {
+                        bili.Add(b);
+                    }
+                }
+            }
+            foreach (Event e in events)
+            {
+                foreach (Building b in bili)
+                {
+                    if (e.id_building == b.id_building)
+                    {
+                        Users.Add(e);
+                    }
+                }
+
+            }
+            return View(Users.ToList());
+
+        }
+        public ActionResult SearchByDate(DateTime? id)
+        {
+            var events = db.Events;
+            List<Event> Users = new List<Event>();
+            foreach (Event e in events)
+            {
+                if (e.date == id)
+                {
+                    Users.Add(e);
+                }
+
+
+            }
+            return View(Users.ToList());
+
+        }
     }
 }
